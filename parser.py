6c80c9b1775f02e8135961c863c06259a44cb2eb
MykolaKlishch <mykola.klishch1707@gmail.com>
2020-05-24 19:06:11 +0300

    verbosity - action="count"

--- parser.py
+++ parser.py
@@ -5,28 +5,13 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("square", type=int,
                     help="display a square of a given number")
-parser.add_argument("-v", "--verbosity", type=int, choices=[0, 1, 2],
+parser.add_argument("-v", "--verbosity", action="count",
                     help="increase output verbosity")
 args = parser.parse_args()
 answer = args.square ** 2
-
-# print("========")
-# print(parser)
-# print("========")
-# print(dir(parser))
-# print("========")
-# print(parser.add_argument)
-# print("========")
-# print(parser.parse_args)
-# print("========")
-# print(parsed_args)
-# print("========")
-# print(parsed_args.echo)
-# print("========")
-# print(argparse.ArgumentParser.mro())
-if args.verbosity == 2:
+if args.verbosity >= 2:
     print(f'the square of {args.square} equals {answer}')
-elif args.verbosity == 1:
+elif args.verbosity >= 1:
     print(f'{args.square}^2 == {answer}')
 else:
     print(answer)
