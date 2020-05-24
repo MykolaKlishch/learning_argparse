bb8663cec6a1a8a1e4fbf5d33bc9d69a3863b9cb
MykolaKlishch <mykola.klishch1707@gmail.com>
2020-05-24 19:09:17 +0300

    verbosity - default=0

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("square", type=int,
                     help="display a square of a given number")
-parser.add_argument("-v", "--verbosity", action="count",
+parser.add_argument("-v", "--verbosity", action="count", default=0,
                     help="increase output verbosity")
 args = parser.parse_args()
 answer = args.square ** 2
