a0bbe155417c7ea274325575551596c68bae1c99
MykolaKlishch <mykola.klishch1707@gmail.com>
2020-05-24 18:52:52 +0300

    two levels of verbosity spcified

--- parser.py
+++ parser.py
@@ -5,7 +5,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("square", type=int,
                     help="display a square of a given number")
-parser.add_argument("-v", "--verbose",
+parser.add_argument("-v", "--verbose", type=int,
                     help="increase output verbosity",
                     action='store_true')
 args = parser.parse_args()
@@ -25,7 +25,9 @@ answer = args.square ** 2
 # print(parsed_args.echo)
 # print("========")
 # print(argparse.ArgumentParser.mro())
-if args.verbose:
+if args.verbose == 2:
     print(f'the square of {args.square} equals {answer}')
+elif args.verbose == 1:
+    print(f'{args.square}^2 == {answer}')
 else:
     print(answer)
