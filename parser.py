477d235216ebccb6bfbb93743e633eaa97d3aa71
MykolaKlishch <mykola.klishch1707@gmail.com>
2020-05-24 19:17:57 +0300

    the exponent added

--- parser.py
+++ parser.py
@@ -3,15 +3,15 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("square", type=int,
-                    help="display a square of a given number")
+parser.add_argument("x", type=int, help="the base")
+parser.add_argument("y", type=int, help="the exponent", default=2)
 parser.add_argument("-v", "--verbosity", action="count", default=0,
                     help="increase output verbosity")
 args = parser.parse_args()
 answer = args.square ** 2
 if args.verbosity >= 2:
-    print(f'the square of {args.square} equals {answer}')
-elif args.verbosity >= 1:
+    print(f"Running '{__file__}' ...")
+if args.verbosity >= 1:
     print(f'{args.square}^2 == {answer}')
 else:
     print(answer)
