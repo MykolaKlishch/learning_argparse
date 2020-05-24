129af86477473419227268ad9ea630bf5c515cb2
MykolaKlishch <mykola.klishch1707@gmail.com>
2020-05-24 17:23:22 +0300

    prints added

--- parser.py
+++ parser.py
@@ -1,4 +1,12 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.parse_args()
+parser.add_argument("echo")
+parsed_args = parser.parse_args()
+
+print("========")
+print(parser)
+print(parser.add_argument)
+print(parser.parse_args)
+print(parsed_args)
+print(parsed_args.echo)
